5837cbc41f2b5cf8f50cfa30a6d64f8e579cf0ad
ericleegit <eric.lee@hkfdt.com>
2017-01-04 22:49:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/as_startanotheractivity/A_Activity.java
@@ -0,0 +1,29 @@
+package com.example.as_startanotheractivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class A_Activity extends Activity implements View.OnClickListener {
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a);
+
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, B_Activity.class);
+        startActivities(new Intent[]{intent});
+    }
+
+}
---
+++ app/src/main/java/com/example/as_startanotheractivity/B_Activity.java
@@ -0,0 +1,13 @@
+package com.example.as_startanotheractivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class B_Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+}
